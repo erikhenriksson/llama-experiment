fe94ab91138617cfe91c3c5ffbc4baf630a0ef45
Erik <eekihenriksson@gmail.com>
2024-10-20 14:04:40 +0300

    Update utils.py

--- llama/models/utils.py
+++ llama/models/utils.py
@@ -15,12 +15,12 @@ class RoPEConfig:
 class LlamaConfig:
     vocab_size: int = 128_256
     emb_dim: int = 768
-    hidden_dim: int = 1048
+    hidden_dim: int = 768
     context_length: int = 1024
     n_layers: int = 12
     n_heads: int = 12
     qkv_bias: bool = False
-    n_kv_groups: int = 4
+    n_kv_groups: int = 8
     rope_base: int = 50_000
     rope_freq: RoPEConfig = RoPEConfig()  # Use the RoPEConfig dataclass here
     dtype: Optional[torch.dtype] = torch.bfloat16  # Optional torch dtype with default
